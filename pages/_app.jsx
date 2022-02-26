dd276723a507e7a8dc00953d13d28dc916af2692
Javier Latorre <jlatorre@ravenpack.com>
2022-02-26 16:49:46 +0100

    Add very simple flex layout

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,20 @@
-import "../styles/globals.scss";
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className="appContainer">
+      <section>
+        <nav>Navigation</nav>
+        <article>
+          <header>Header</header>
+          <section className="pageContent">
+            <Component {...pageProps} />
+          </section>
+        </article>
+      </section>
+      <footer>Footer</footer>
+    </main>
+  );
 }
 
 export default MyApp;
